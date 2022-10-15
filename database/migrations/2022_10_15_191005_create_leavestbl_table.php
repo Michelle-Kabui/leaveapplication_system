ca882986cc3451d980669bb246200aebe2d6f3e7
chivunira <emmanuelchivunira@gmail.com>
2022-10-15 22:31:59 +0300

    2

--- database/migrations/2022_10_15_191005_create_leavestbl_table.php
+++ database/migrations/2022_10_15_191005_create_leavestbl_table.php
@@ -13,15 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('leavestbl', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string("department");
-            $table->integer("av_days");
-            $table->rememberToken();
+            $table->integer("userID");
+            $table->string("leavetype");
+            $table->date("ToDate");
+            $table->date("FromDate");
+            $table->string("Description");
+            $table->string("Status");
+            $table->string("AdminRemarks");
+            $table->integer("numDays");
             $table->timestamps();
         });
     }
@@ -33,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('leavestbl');
     }
 };
--- database/migrations/2022_10_15_192455_create_leavetype_table.php
+++ database/migrations/2022_10_15_192455_create_leavetype_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('leavetype', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string("department");
-            $table->integer("av_days");
-            $table->rememberToken();
+            $table->string("LeaveType");
+            $table->string("Description");
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('leavetype');
     }
 };
